d964f066564710d109cefb8f66a4f7c7759df88f
Dago23 <jordidape@gmail.com>
2026-03-10 20:26:27 +0100

    retoques del proyecto

--- EcoNexo/frontend/src/app/core/component/layout/header/header.ts
+++ EcoNexo/frontend/src/app/core/component/layout/header/header.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +8,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+
+export class Header {
+  protected readonly router = inject(Router);
+}
