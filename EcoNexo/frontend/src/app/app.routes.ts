71093dd0163718a5e2566ccf797537eb734112eb
Miquelsimoz24 <miquelsimoz@gmail.com>
2026-03-10 16:50:36 +0100

    bug
    avía un error

--- EcoNexo/frontend/src/app/app.routes.ts
+++ EcoNexo/frontend/src/app/app.routes.ts
@@ -3,11 +3,8 @@ import { Login } from './login/login';
 import { RegisterCliente } from './register-cliente/register-cliente';
 import { RegisterNegocio } from './register-negocio/register-negocio';
 import { Home } from './home/home';
-<<<<<<< Updated upstream
 import { Negocios } from './negocios/negocios';
-=======
 import { About } from './about/about';
->>>>>>> Stashed changes
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -16,9 +13,6 @@ export const routes: Routes = [
   { path: 'register/cliente', component: RegisterCliente },
   { path: 'register/negocio', component: RegisterNegocio },
   { path: 'home', component: Home },
-<<<<<<< Updated upstream
   { path: 'negocios', component: Negocios },
-=======
   { path: 'about', component: About },
->>>>>>> Stashed changes
 ];
