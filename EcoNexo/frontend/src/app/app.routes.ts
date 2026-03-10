48a7118070780600a19bbb705fe437c355947ba5
Miquelsimoz24 <miquelsimoz@gmail.com>
2026-03-10 16:47:35 +0100

    creacion
    creacion de la pantalla de sobre nosotros

--- EcoNexo/frontend/src/app/app.routes.ts
+++ EcoNexo/frontend/src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { Login } from './login/login';
 import { RegisterCliente } from './register-cliente/register-cliente';
 import { RegisterNegocio } from './register-negocio/register-negocio';
 import { Home } from './home/home';
+<<<<<<< Updated upstream
 import { Negocios } from './negocios/negocios';
+=======
+import { About } from './about/about';
+>>>>>>> Stashed changes
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -12,5 +16,9 @@ export const routes: Routes = [
   { path: 'register/cliente', component: RegisterCliente },
   { path: 'register/negocio', component: RegisterNegocio },
   { path: 'home', component: Home },
+<<<<<<< Updated upstream
   { path: 'negocios', component: Negocios },
+=======
+  { path: 'about', component: About },
+>>>>>>> Stashed changes
 ];
